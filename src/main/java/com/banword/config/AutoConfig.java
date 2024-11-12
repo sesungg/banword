11ba4fd00d8b23eec8d8c88dc48d060d71648505
sesungg <sesungg@gmail.com>
2024-11-12 14:50:32 +0900

    feat : 빈 등록 설정

---
+++ src/main/java/com/banword/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.banword.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.banword")
+public class AutoConfig {
+}
